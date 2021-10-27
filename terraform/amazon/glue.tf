3a940fec461fce4938a05224ce8178b4ffbf1785
Jonathan Shin <jonathan.shin@soundcloud.com>
2021-10-27 10:55:44 -0700

    Attempt resource creation without count

--- terraform/amazon/glue.tf
+++ terraform/amazon/glue.tf
@@ -2,6 +2,7 @@ locals {
   glue_script_key = "admin/${var.glue_job_name}.py"
 }
 
+# TODO: Upload sample file for crawler to generate schema
 resource "aws_s3_bucket_object" "glue_script" {
   bucket = var.glue_script_bucket
   key    = local.glue_script_key
@@ -32,12 +33,12 @@ resource "aws_glue_job" "glue_job_with_connection" {
 }
 
 resource "aws_glue_catalog_database" "glue_catalog_database" {
-  count = tobool(var.crawler_required) ? 0 : 1
+  # count = tobool(var.crawler_required) ? 0 : 1
   name = var.source_table_name 
 }
 
 resource "aws_glue_crawler" "glue_crawler" {
-  count = tobool(var.crawler_required) ? 0 : 1
+  # count = tobool(var.crawler_required) ? 0 : 1
   schedule = var.crawler_schedule
   name = var.crawler_name #repost-staging-market-analytics-spotify-crawler
   role = var.glue_job_role_arn
